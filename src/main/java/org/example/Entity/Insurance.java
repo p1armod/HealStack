49437c7f948b8a607633e3e4bd29c846ae7ef1a7
p1armod <parmodrns@gmail.com>
2025-12-02 13:16:59 +0530

    Created Insurance Entity

---
+++ src/main/java/org/example/Entity/Insurance.java
@@ -0,0 +1,36 @@
+package org.example.Entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Insurance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String policyNumber;
+
+    private String provider;
+
+    private LocalDate validUntil;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
