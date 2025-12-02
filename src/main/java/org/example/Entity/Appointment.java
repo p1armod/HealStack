4d7cfa73249b702ceb4f17128f17d93a97432085
p1armod <parmodrns@gmail.com>
2025-12-02 13:16:24 +0530

    Created Appointment Entity

---
+++ src/main/java/org/example/Entity/Appointment.java
@@ -0,0 +1,40 @@
+package org.example.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime appointmentTime;
+
+    private String reason;
+
+    private String status;
+
+
+
+
+
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
