e7f85a0c504098a0a8e9f80e76f5909d4ba7de9f
p1armod <parmodrns@gmail.com>
2025-12-02 13:16:11 +0530

    Created Patient Entity

---
+++ src/main/java/org/example/Entity/Patient.java
@@ -0,0 +1,47 @@
+package org.example.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.Entity.Type.BloodGroupType;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+
+    public BloodGroupType bloodGroupType;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+
+}
